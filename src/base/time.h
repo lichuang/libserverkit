3d1b3e67b19e57848d76801cb054ea225b9170ed
lichuang <lichuang1982@gmail.com>
2019-06-05 19:24:08 +0800

    ci

---
+++ src/base/time.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) codedump
+ */
+#ifndef __SERVERKIT_BASE_TIME_H__
+#define __SERVERKIT_BASE_TIME_H__
+
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <string>
+
+using namespace std;
+
+namespace serverkit {
+
+struct timespec *MakeTimespec(int64_t ms, struct timespec *ts);
+uint64_t NowMs();
+void NowMsString(string *ret);
+
+};  // namespace serverkit
+
+#endif  // __SERVERKIT_BASE_TIME_H__
