5f62851a30175aa2eb26f73cc580870959a6458f
lichuang <lichuang1982@gmail.com>
2019-07-24 22:59:44 +0800

    add cmake files

---
+++ libserverkit.cmake
@@ -0,0 +1,24 @@
+set(libserverkit_files
+  src/base/buffer.cc
+  src/base/clock.cc
+  src/base/condition.cc
+  src/base/error.cc
+  src/base/lock.cc
+  src/base/log.cc
+  src/base/log_stream.cc
+  src/base/net.cc
+  src/base/string.cc
+  src/base/thread.cc
+  src/base/time.cc
+  src/core/epoll.cc
+  src/core/io_thread.cc
+  src/core/listener.cc
+  src/core/mailbox.cc
+  src/core/poller.cc
+  src/core/server.cc
+  src/core/signaler.cc
+  src/core/socket.cc
+)
+
+add_library(serverkit ${serverkit_SHARED_OR_STATIC}
+  ${libserverkit_files})
