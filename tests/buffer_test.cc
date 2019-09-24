12ea891c8c1d127465fd9ab7ffdecef22ddbc72a
lichuang <lichuang1982@gmail.com>
2019-09-24 18:14:44 +0800

    refactor buffer add tests

---
+++ tests/buffer_test.cc
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) codedump
+ */
+
+#include <string.h>
+#include <gtest/gtest.h>
+#include "base/buffer.h"
+
+using namespace serverkit;
+
+TEST(BufferTest, BufferTest) {
+  Buffer buf;
+
+  EXPECT_EQ(buf.Length(), 0);
+  EXPECT_EQ(buf.Available(), kBufferSize);
+
+  string str = "hello";
+  buf.AppendString(str);
+  EXPECT_EQ(buf.Length(), str.length());
+  EXPECT_EQ(buf.Available(), kBufferSize - str.length());
+
+  buf.Reset();
+  EXPECT_EQ(buf.Length(), 0);
+  EXPECT_EQ(buf.Available(), kBufferSize);  
+}
+
+TEST(BufferListTest, testBufferList) {
+  BufferList buf;
+
+  EXPECT_EQ(buf.ReadableSize(), 0);
+  EXPECT_EQ(buf.WriteableSize(), kBufferSize);
+  EXPECT_TRUE(buf.Empty());
+
+  string str = "hello";
+  buf.WriteString(str);
+  EXPECT_EQ(buf.ReadableSize(), str.length());
+  EXPECT_EQ(buf.WriteableSize(), kBufferSize - str.length());
+  EXPECT_FALSE(buf.Empty());
+
+  char data[10];
+  size_t n = buf.Read(data, sizeof(data));
+  data[n] = '\0';
+  EXPECT_EQ(str, data);
+}
+
+TEST(BufferListTest, testLargeBuffer) {
+  BufferList buf;
+  char data[4 * kBufferSize];
+
+  string tmp;
+  string str = "hello";
+  size_t n = 0;
+  while (n < 3 * kBufferSize) {
+    n += buf.Write(str.c_str(), str.length());
+    tmp += str;
+  }
+
+  EXPECT_EQ(buf.WriteSize(), n);
+  EXPECT_EQ(buf.ReadSize(), 0);
+  EXPECT_EQ(buf.ReadableSize(), n);
+
+  size_t read = buf.Read(data, sizeof(data));
+  EXPECT_EQ(read, n);
+  data[read] = '\0';
+  EXPECT_EQ(tmp, data);
+}
