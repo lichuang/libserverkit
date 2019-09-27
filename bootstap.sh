942197fb97d09524e7113a34121c27eab23fb278
codedump <lichuang1982@gmail.com>
2019-09-28 00:17:00 +0800

    add gtest

--- bootstap.sh
+++ bootstap.sh
@@ -3,12 +3,31 @@
 pwd=`pwd`
 third_party=${pwd}/third_party
 pb=protobuf-3.9.0
+gtest=googletest-release-1.8.0
 
 echo "$pwd"
 echo "${third_party}"
 
+mkdir ${third_party}/include -p
+mkdir ${third_party}/lib -p
+
 cd $pwd/deps
 
+echo "compile gtest..."
+rm -fr ${gtest}
+tar xvf ${gtest}.tar.gz
+cd ${gtest}
+cmake .
+make -j8
+cp ./googlemock/libgmock* ${third_party}/lib/
+cp ./googlemock/gtest/libgtest* ${third_party}/lib/
+cp -r googlemock/include/gmock ${third_party}/include/
+cp -r googletest/include/gtest ${third_party}/include/
+cd ../
+rm -fr ${gtest}
+echo "compile gtest done"
+
+cd $pwd/deps
 echo "compile protobuf..."
 rm -fr ${pb}
 tar xvf ${pb}.tar.gz
