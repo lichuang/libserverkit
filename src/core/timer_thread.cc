224efdb47038a2f83d8c7722ce7b9d9c6d752aaa
lichuang <lichuang1982@gmail.com>
2019-10-17 09:58:47 +0800

    add timer thread

--- src/core/timer_thread.cc
+++ src/core/timer_thread.cc
@@ -4,6 +4,7 @@
 
 #include <time.h>
 #include <sys/time.h>
+#include "base/time.h"
 #include "core/timer_thread.h"
 
 namespace serverkit {
@@ -19,16 +20,23 @@ TimerThread::~TimerThread() {
 }
 
 void 
+TimerThread::doInit() {
+
+}
+
+void 
 TimerThread::updateTime() {
-  volatile Time *tp;
-  volatile uint64_t *tmsec;
+  Time *tp;
+  uint64_t *tmsec;
+  char* logStr;
   time_t sec;
   uint64_t msec;
 
   index_ = (index_ + 1) % kTimeSlots;
 
-  tp = &cached_time_[index_];
+  tp = &cached_times_[index_];
   tmsec = &cached_msecs_[index_];
+  logStr = &(cached_log_time_strs_[index_][0]);
 
   struct timeval tv;
   gettimeofday (&tv, NULL);
@@ -39,6 +47,18 @@ TimerThread::updateTime() {
   *tmsec = sec * 1000 + msec;
   tp->sec = sec;
   tp->msec = msec;
+
+  struct tm tim;
+	Localtime(tv.tv_sec, &tim);
+  
+  snprintf(logStr, kLogTimeStringLength,
+    "%4d/%02d/%02d %02d:%02d:%02d.%03d",
+    tim.tm_year + 1900, tim.tm_mon + 1, tim.tm_mday,
+    tim.tm_hour, tim.tm_min, tim.tm_sec, static_cast<int>(msec)); 
+
+  cached_time_ = tp;
+  cached_msec_ = tmsec;
+  cached_log_time_str_ = logStr;
 }
 
 void
--- src/core/timer_thread.h
+++ src/core/timer_thread.h
@@ -7,6 +7,7 @@
 
 #include <stdint.h>
 #include <time.h>
+#include "base/singleton.h"
 #include "base/thread.h"
 
 namespace serverkit {
@@ -23,9 +24,13 @@ static const int kTimeSlots = 64;
 // update internal msec
 static const int kUpdateIntervalMsec = 50;
 
+// log time string length
+static const int kLogTimeStringLength = sizeof("2019/10/01 00:00:00.000");
+
 class TimerThread : public Thread {
+  friend class Singleton<TimerThread>;
+
 public:
-  TimerThread();
   virtual ~TimerThread();
 
   // msec since 1970.01.01 00:00:00
@@ -33,21 +38,34 @@ public:
     return *cached_msec_;
   }
 
+  char* GetNowLogtimeString() const {
+    return const_cast<char*>(cached_log_time_str_);
+  }
+
 private:
+  TimerThread();
   void updateTime();
+  void doInit();
 
 protected:
   virtual void Run();
 
 private:
   Time cached_times_[kTimeSlots];
+  char cached_log_time_strs_[kTimeSlots][kLogTimeStringLength];
   uint64_t cached_msecs_[kTimeSlots];
 
   volatile Time* cached_time_;
+  volatile char* cached_log_time_str_;
   volatile uint64_t* cached_msec_;
 
   int index_;
 };
+
+#define gTimerThread serverkit::Singleton<serverkit::TimerThread>::Instance()
+
+#define CurrentMs gTimerThread->GetNowMs
+#define CurrentLogtimeString gTimerThread->GetNowLogtimeString
 
 };  // namespace serverkit
 
